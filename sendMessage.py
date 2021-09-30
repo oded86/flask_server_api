a62295e5d1ad0c2edb1394e7b53e30b79ae0c00c
Sagie <sageika@gmail.com>
2021-09-30 15:41:31 +0300

    without_sendMessage_following

--- sendMessage.py
+++ sendMessage.py
@@ -13,8 +13,8 @@ def sendMessage(message, adress):
         token='1702864193:AAGj_J3ipAQ3wS0P1a2GEoEnnaAXOx9f9FY', use_context=True)
     updater.bot.sendPhoto(
         chat_id='-519827084', photo='https://merrimackvalleytma.com/wp-content/uploads/megamenu_image_2-160812-143410-emergency-notification-image.jpg')
-    updater.bot.sendMessage(chat_id='-519827084',
-                            text='the deatils: '+message+' in the loccation:')
+    # updater.bot.sendMessage(chat_id='-519827084',
+    #                         text='the deatils: '+message+' in the loccation:')
     # updater.bot.sendMessage(chat_id='-519827084', text=adress)
     updater.bot.sendLocation(chat_id='-519827084',
                              latitude=32.0123111,
@@ -40,9 +40,9 @@ def sendMessageDogCat(message, adress):
         token='1702864193:AAGj_J3ipAQ3wS0P1a2GEoEnnaAXOx9f9FY', use_context=True)
     updater.bot.sendPhoto(
         chat_id='-519827084', photo='https://merrimackvalleytma.com/wp-content/uploads/megamenu_image_2-160812-143410-emergency-notification-image.jpg')
-    updater.bot.sendMessage(chat_id='-519827084',
-                            # text='The details: the system has recognized a dog in the following location:')
-                            text=message+"\n the following location:")
+    # updater.bot.sendMessage(chat_id='-519827084',
+    #                         # text='The details: the system has recognized a dog in the following location:')
+    #                         text=message+"\n the following location:")
     # updater.bot.sendMessage(chat_id='-519827084', text=adress)
     updater.bot.sendLocation(chat_id='-519827084',
                              latitude=float(adress[0]),
